22089f72d2284fee989a3a13502fd02a50123199
John Keller <jmkeller3@gmail.com>
2018-05-16 13:48:57 -0500

    added all files from quiz all

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+        question: 'According to legend, who was the first person to discover the coffee plant?',
+        answers: [
+            'Kaldi, a Goat Herder',
+            'Christopher Columbus',
+            'Simba, King of Ethiopia',
+            'Julius Caesar',
+        ],
+        correctAnswer: 'Kaldi, a Goat Herder',
+    },
+    {
+        question: 'Which Government required men to provide coffee for their wives or be threatened with divorce?',
+        answers: [
+            '1st Century Roman Empire',
+            '16th Century Ottoman Turkish Empire',
+            '17th Century Colonial British Empire',
+            '20th Century Soviet Union',
+        ],
+        correctAnswer: '16th Century Ottoman Turkish Empire',
+    },
+    {
+        question: 'When was the first Espresso Machine invented?',
+        answers: [
+            '1912',
+            '1933',
+            '1942',
+            '1970',
+        ],
+        correctAnswer: '1933',
+    },
+    {
+        question: 'Which coffee bean is considered to be superior in quality and flavor?',
+        answers: [
+            'Arabica',
+            'Cafe',
+            'Espresso',
+            'Robusta',
+        ],
+        correctAnswer: 'Arabica',
+    },
+    {
+        question: 'Latin American Coffees are processed primarily by which method?',
+        answers: [
+            'Natural Processing',
+            'Semi-Washed Processing',
+            'Simple Processing',
+            'Washed Processing',
+        ],
+        correctAnswer: 'Washed Processing',
+    },
+    {
+        question: 'What is one unique benefit to the natural processing the coffee bean?',
+        answers: [
+            'It keeps the fruit intact while obtaining the coffee bean.',
+            'The coffee bean often has a more  fruity and juicy flavor.',
+            'It increases the caffeine content greatly.',
+            'It has no significant difference than other processing methods which use water.',
+        ],
+        correctAnswer: 'The coffee bean often has a more  fruity and juicy flavor.',
+    },
+    {
+        question: 'How many times will a coffee tree produce coffee cherries?',
+        answers: [
+            'Once a decade',
+            'Once a year',
+            'Once a month',
+            'Once a week',
+        ],
+        correctAnswer: 'Once a year',
+    },
+    {
+        question: 'How many years can a coffee tree stay productive with proper care?',
+        answers: [
+            '1-5 years',
+            '5-10 years',
+            '20-25 years',
+            '40 years',
+        ],
+        correctAnswer: '20-25 years'
+    },
+    {
+        question: 'Which geographical region is known for its full bodied and “spicy” coffee flavors?',
+        answers: [
+            'Africa/Arabia',
+            'Asia/Pacific',
+            'Europe',
+            'Latin America',
+        ],
+        correctAnswer: 'Asia/Pacific',
+    },
+    {
+        question: 'What is the ideal elevation to grow quality coffee trees?',
+        answers: [
+            '0-400 meters',
+            '500-800 meters',
+            '900-1,800 meters',
+            'Elevation has no effect on coffee bean quality.',
+        ],
+        correctAnswer: '900-1,800 meters',
+    }
+];
